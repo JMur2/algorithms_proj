33653faadecc6e31f0d026d5b676931fa561cb27
JMur2 <jakob.murray@marquette.edu>
2021-04-05 10:52:53 -0500

    organized code nicely

--- least_common_subsequence.py
+++ least_common_subsequence.py
@@ -1,34 +1,58 @@
+import pandas as pd
 
-def least_common_subsequence(X, Y):
-    """
-    This algorithm checks for the longest common subsequence between 2 arrays
+class Project:
 
-    Parameters:
-    X: array of values
-    Y: array of values
-    """
+    def __init__(self):
+        pass
 
-    len_x = len(X)
-    len_y = len(Y)
+    def least_common_subsequence(self, X, Y):
+        """
+        This algorithm checks for the longest common subsequence between 2 arrays
 
-    L = [[None]*(len_y + 1) for i in range(len_x + 1)]
+        Solved without using recursion
 
-    for i in range(len_x+1):
-        for j in range(len_y+1):
-            if i == 0 or j == 0:
-                L[i][j] = 0
-            elif X[i-1] == Y[j-1]:
-                L[i][j] = L[i-1][j+1] + 1
-            else:
-                L[i][j] = max(L[i-1][j], L[i][j-1])
+        Parameters:
+        X: array of values
+        Y: array of values
+        """
+
+        len_x = len(X)
+        len_y = len(Y)
+
+        L = [[None]*(len_y + 1) for i in range(len_x + 1)]
+
+        for i in range(len_x+1):
+            for j in range(len_y+1):
+                if i == 0 or j == 0:
+                    L[i][j] = 0
+                elif X[i-1] == Y[j-1]:
+                    L[i][j] = L[i-1][j+1] + 1
+                else:
+                    L[i][j] = max(L[i-1][j], L[i][j-1])
+        
+        return L[len_x][len_y]
+        #return L
+
+    def lcs(self, X, Y, m, n):
+        """
+        This algorithm checks for the longest common subsequence between 2 arrays
+
+        Solved with recursion
+
+        Parameters:
+        X: array of values
+        Y: array of values
+        m: size of array X
+        n: size of array Y
+        """
     
-    return L[len_x][len_y]
-    #return L
-
+        if m == 0 or n == 0:
+            return 0;
+        elif X[m-1] == Y[n-1]:
+            return 1 + lcs(X, Y, m-1, n-1);
+        else:
+            return max(lcs(X, Y, m, n-1), lcs(X, Y, m-1, n));
 
 if __name__ == "__main__":
-    X = [20, 10, 9, 8]
-    Y = [20, 3, 9, 77]
-
-
-    print(least_common_subsequence(X, Y))
+    
+    project = Project()
