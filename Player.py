bc491afccf178e6a3a7cd96e77796342da909d31
JMur2 <jakob.murray@marquette.edu>
2021-04-05 19:27:17 -0500

    Added comments

--- Player.py
+++ Player.py
@@ -1,5 +1,9 @@
 class Player:
-
+    """
+    A class that will hold a player's stats for both pre and post moving to a new team.
+    This class will also handle the conversion of stats from their float values to the
+    categories (Good, Neutral, and Bad)
+    """
     def __init__(self, age, pos, trb, ast, stl, blk, pts):
         self.player_id
         self.pre_stats = []
--- least_common_subsequence.py
+++ least_common_subsequence.py
@@ -4,6 +4,7 @@ from openpyxl import load_workbook
 class Project:
 
     def __init__(self):
+        # data frames for storing player data
         self.pre_move_df = None
         self.post_move_df = None
 
@@ -11,6 +12,7 @@ class Project:
         wb = load_workbook(filename='data.xlsx', read_only=True)
         ws = wb['Sheet1']
 
+        # selects the stats of players before they moved teams
         data_rows = []
         for row in ws['C4':'I63']:
             data_cols = []
@@ -20,6 +22,7 @@ class Project:
         
         self.pre_move_df = pd.DataFrame(data_rows)
 
+        # selects the stats of players after they moved teams
         data_rows = []
         for row in ws['K4':'Q63']:
             data_cols = []
@@ -27,7 +30,7 @@ class Project:
                 data_cols.append(cell.value)
             data_rows.append(data_cols)
         
-        post_move_df = pd.DataFrame(data_rows)
+        self.post_move_df = pd.DataFrame(data_rows)
         
 
     def least_common_subsequence(self, X, Y):
@@ -60,6 +63,8 @@ class Project:
     def lcs(self, X, Y, m, n):
         """
         This algorithm checks for the longest common subsequence between 2 arrays
+        and includes a modification that places a higher weights on what will be
+        a "good stat" on a player
 
         Solved with recursion
 
@@ -72,18 +77,20 @@ class Project:
             return 0
         elif X[m-1] == Y[n-1]:
             if X[m-1] == 0:
+                # a neutral stat is considered as normal within the LCS
                 return 1 + self.lcs(X, Y, m-1, n-1)
             elif X[m-1] == 1:
+                # a good stat will recieve a higher value to show its importance in the LCS
                 return 2 + self.lcs(X, Y, m-1, n-1)
         else:
             return max(self.lcs(X, Y, m, n-1), self.lcs(X, Y, m-1, n))
 
 if __name__ == "__main__":
+    # used for testing data outputs and LCS outputs - will be more robust as more is added
     project = Project()
-    project.read_data()
+    #project.read_data()
 
-    # x = [0, 1, 1, 1, 0, 1]
-    # y = [0, -1, 0, 1, 1, 0]
-    # z = [-1, 0, -1, 0, 1, 1]
+    x = [1, 0, 1, 1, 0, 1]
+    z = [0, 0, -1, 1, 1, 1]
 
-    # print(project.lcs(x, z, len(x), len(z)))
+    print(project.lcs(x, z, len(x), len(z)))
