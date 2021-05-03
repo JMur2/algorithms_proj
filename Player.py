7f81a674010b89ee8dd5efc59fb148383bd044f2
JMur2 <jakob.murray@marquette.edu>
2021-05-03 17:11:01 -0500

    adjusted layout and added comments

--- Player.py
+++ Player.py
@@ -5,11 +5,28 @@ class Player:
     categories (Good, Neutral, and Bad)
     """
     def __init__(self, id):
+        # player id to help identify
         self.player_id = id
+
+        # arrays for player stats and stat scores
         self.pre_stats = []
         self.post_stats = []
         self.pre_scores = []
         self.post_scores = []
+
+    def set_pre_stat_scores(self):
+        # TODO: complete this function
+        return self.pre_scores
+    
+    def set_post_stat_scores(self):
+        # TODO: complete this function
+        return self.post_scores
+
+    """
+    Below are 5 helper functions that return a value to the parent class, or will set a value within the current player
+    """
+    def get_player_id(self):
+        return self.player_id
     
     def set_pre_move_stats(self, stats):
         self.pre_stats = stats
@@ -22,11 +39,3 @@ class Player:
     
     def get_post_move_stats(self):
         return self.post_stats
-    
-    def set_pre_stat_scores(self):
-        # TODO: complete this function
-        return self.pre_scores
-    
-    def set_post_stat_scores(self):
-        # TODO: complete this function
-        return self.post_scores
